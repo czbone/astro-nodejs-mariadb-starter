492a658fa3457ba9ae99b150b8766f6bcd45ce4d
naoki hirata <naoki@magic3.org>
2025-12-04 09:37:11 +0900

    設定ファイル更新。

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,16 @@ import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
   // SSR type configuration
   output: 'server',
   adapter: node({
     mode: 'standalone'
   }),
-  server: { port: 3000, host: true /* ホスティング時必須 */ },
+  server: {
+    port: 3000, // 使用したいポート番号に変更
+    host: true // 外部からのアクセスを許可する場合
+  },
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
